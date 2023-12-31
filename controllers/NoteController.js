38f481bd974616c772acfd57b9f881c60aed8625
unknown <mohammadahmermalick@gmail.com>
2023-12-31 22:36:26 +0400

    feat: create note form status updated and ui updated

--- controllers/NoteController.js
+++ controllers/NoteController.js
@@ -7,11 +7,11 @@ const noteView = (req, res) => {
 const createNote = async (req, res) => {
   const { title, note } = req.body
 
-  if (!note) return res.render('note')
+  if (!note) return res.render('note/note', { reqStatus: 'error', reqMessage: 'Note is required' })
 
   const newNote = new Note({ title, note })
   await newNote.save()
-  return res.render('note/note')
+  return res.render('note/note', { reqStatus: 'success', reqMessage: 'Note Saved' })
 }
 
 module.exports = { noteView, createNote }
